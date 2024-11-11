60b74705a9da0b7060c4a5ad19ba35008e1ea762
sensen <85843654+yutsunoki@users.noreply.github.com>
2024-11-11 12:38:33 +0800

    Update SSH_via_BASH.sh

--- SSH_via_BASH.sh
+++ SSH_via_BASH.sh
@@ -1,5 +1,5 @@
 <<_comment
-This trick I use a lot from handling data, and if I'm lazy to upload script & run via scp.
+This trick I use a lot for handling data, and if I'm lazy to upload script & run via scp.
 _comment
 
 ssh root@examplehost.com -- bash <<BASH
